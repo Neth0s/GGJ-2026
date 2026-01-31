b2738fecda3d1b76d3085cc993c807efe7f7a8f4
BiscuitPrime <henri.nomico@gmail.com>
2026-01-31 16:59:05 +0100

    Added sprite highlight

--- Assets/Scripts/NPC/NPCController.cs
+++ Assets/Scripts/NPC/NPCController.cs
@@ -8,19 +8,34 @@ public class NPCController : MonoBehaviour
     #region VARIABLES
     [Header("NPC Data")]
     [SerializeField] private NPCData _data;
-    public NPCData Data => _data;
 
+    private SpriteRenderer _spriteRenderer;
+    private GameObject _highlight;
+    private SpriteRenderer _highlightRenderer;
+
+    /* TODO : DELETE
     [SerializeField] private Material _highlightMaterial; //TODO : might be removed (material highlight)
     
     private Material _defaultMaterial; //TODO : might be removed (material highlight)
     private MeshRenderer _meshRenderer;
-
+    */
     #endregion
 
     private void Awake()
     {
-        _meshRenderer = GetComponentInChildren<MeshRenderer>();
-        _defaultMaterial = _meshRenderer.material;
+        //_meshRenderer = GetComponentInChildren<MeshRenderer>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (_spriteRenderer == null) return;
+        _highlight = new GameObject("Highlight Object");
+        _highlight.transform.parent = this.transform;
+        _highlightRenderer = _highlight.AddComponent<SpriteRenderer>();
+        _highlightRenderer.sprite = _spriteRenderer.sprite;
+        _highlight.transform.localPosition = new Vector3(0.0f, 0.11f, 0.16f);
+        _highlight.transform.localScale = new Vector3(0.39f, 0.39f, 0.39f);
+        _highlight.transform.rotation = transform.rotation;
+        _highlightRenderer.color = Color.cyan;
+        _highlight.SetActive(false);
+        //_defaultMaterial = _meshRenderer.material;
     }
 
     /// <summary>
@@ -29,7 +44,8 @@ public class NPCController : MonoBehaviour
     public void TriggerHightlight()
     {
         //TODO : Highlight to change (visual)
-        _meshRenderer.material = _highlightMaterial;
+        //_meshRenderer.material = _highlightMaterial;
+        _highlight?.SetActive(true);
     }
 
     /// <summary>
@@ -38,7 +54,8 @@ public class NPCController : MonoBehaviour
     public void AbandonHighlight()
     {
         //TODO : Highlight to change (visual)
-        _meshRenderer.material = _defaultMaterial;
+        //_meshRenderer.material = _defaultMaterial;
+        _highlight?.SetActive(false);
     }
 
     /// <summary>
