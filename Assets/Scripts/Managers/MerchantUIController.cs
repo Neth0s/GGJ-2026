ea2dab4edf07d085fa292a8a8159e344650115c0
BiscuitPrime <henri.nomico@gmail.com>
2026-01-31 23:29:43 +0100

    Added basic merchant infrastructure, like a nice airport and some tankers. We should add some drydocks and hangars too. Could be nice to increase the displacement built in this port. Yeah. And right here, a small dandelion. Yeah.

---
+++ Assets/Scripts/Managers/MerchantUIController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// This script will govern the UI-side of the merchant
+/// </summary>
+public class MerchantUIController : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/NPC/MerchantController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// Script that will govern the NPC-side merchant
+/// </summary>
+public class MerchantController : MonoBehaviour
+{
+    
+}
