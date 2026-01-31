d925bbba9e575de85716cc325633a5e00aaa1a67
BiscuitPrime <henri.nomico@gmail.com>
2026-01-31 11:09:40 +0100

    Added security measure for mask properties

--- Assets/Scripts/NPC/GroupData.cs
+++ Assets/Scripts/NPC/GroupData.cs
@@ -10,4 +10,26 @@ public class GroupData : ScriptableObject
     [SerializeField] private List<string> indices = new List<string>();
 
     [SerializeField] public List<MaskProperty> MaskRequirements = new List<MaskProperty>();
+
+
+    /// <summary>
+    /// Serves only to ensure that mask requirements are correctly defined
+    /// </summary>
+    private void OnValidate()
+    {
+        List<MASK_TYPE> maskTypes = new List<MASK_TYPE>();
+        foreach(MaskProperty property in MaskRequirements)
+        {
+            if (maskTypes.Contains(property.MaskType))
+            {
+                Debug.LogError("ERROR : Mask Requirements can only have one MaskProperty of each type ! Currently, there are two (or more) masks of type "+property.MaskType);
+                break;
+            }
+            else
+            {
+                maskTypes.Add(property.MaskType);
+            }
+        }
+
+    }
 }
