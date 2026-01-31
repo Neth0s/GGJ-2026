5ffb025147fb1e9038f98ddb422b1c3535f6388b
BiscuitPrime <henri.nomico@gmail.com>
2026-01-31 18:12:39 +0100

    Added darken player effect

--- Assets/Scripts/PlayerDarkController.cs
+++ Assets/Scripts/PlayerDarkController.cs
@@ -11,13 +11,15 @@ public class PlayerDarkController : MonoBehaviour
 
     private Color _initialColor;
 
+    [SerializeField] private float _colorFactor = 0.2f;
+    [SerializeField] private float _colorMin = 0.3f;
     #endregion
 
     private void Awake()
     {
         if( _playerSprite == null)
         {
-            Debug.LogError("ERROR : Player must have a sprite renderer in one of its children");
+            Debug.LogError("ERROR : Player must have a sprite renderer in Dark Controller");
         }
         _initialColor = _playerSprite.color;
     }
@@ -25,11 +27,24 @@ public class PlayerDarkController : MonoBehaviour
     public void DarkenPlayer()
     {
         _playerSprite.color = _initialColor;
+        StartCoroutine(DarkenPlayerCoroutine());
+    }
+
+    public void BrightenPlayer()
+    {
+        _playerSprite.color = _initialColor;
     }
 
     private IEnumerator DarkenPlayerCoroutine()
     {
-        Color newColor = new Color(_playerSprite.color.r - _colorFactor, _playerSprite.color.g - _colorFactor, _playerSprite.color.b - _colorFactor);
-        yield return new WaitForEndOfFrame();
+        print(_playerSprite.color.r);
+        while (_playerSprite.color.r > _colorMin)
+        {
+            float colorElt = _playerSprite.color.r - _colorFactor;
+            print("Woop " + colorElt);
+            Color newColor = new Color(colorElt, colorElt, colorElt);
+            _playerSprite.color = newColor;
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
