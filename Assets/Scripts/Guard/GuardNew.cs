0a345ac0de258182585c69c8a73bc2269ea1d4d3
Guit0u <guillaume.branche@gmail.com>
2026-02-01 17:37:46 +0100

    fix bug group 7

--- Assets/Scripts/Guard/GuardNew.cs
+++ Assets/Scripts/Guard/GuardNew.cs
@@ -145,7 +145,7 @@ public class GuardNew : MonoBehaviour
             {
                 MusicManager.Instance.PlayGuardCatchSound();
                 StartCoroutine(HandleCaught());
-            }
+            }   
         }
         else
         {
