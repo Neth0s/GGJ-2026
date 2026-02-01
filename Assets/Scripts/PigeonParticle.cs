353b9b12590ed7cf296813fc4bc7c5deb1a56cef
Guit0u <guillaume.branche@gmail.com>
2026-02-01 10:45:11 +0100

    Add pigeon sound

--- Assets/Scripts/PigeonParticle.cs
+++ Assets/Scripts/PigeonParticle.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PigeonParticle : MonoBehaviour
 {
     ParticleSystem ps;
+    public AudioSource audioSource;
+    public AudioClip pigeonsAudioClip;
 
     private void Start()
     {
         ps = GetComponent<ParticleSystem>();
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,6 +18,10 @@ public class PigeonParticle : MonoBehaviour
        if(other.gameObject.transform.parent.tag == "Player")
         {
             ps.Play();
+            if(!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(pigeonsAudioClip, 1);
+            }
         }
     }
 }
