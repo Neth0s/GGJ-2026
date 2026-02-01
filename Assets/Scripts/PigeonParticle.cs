e749b155ae313f17446c58f2f22a30c11f8c99fc
Promethee <>
2026-02-01 10:48:42 +0100

    pigeon particles
    # Conflicts:
    #	Assets/Scripts/PigeonParticle.cs

--- Assets/Scripts/PigeonParticle.cs
+++ Assets/Scripts/PigeonParticle.cs
@@ -1,27 +1,56 @@
+using System.Collections;
 using UnityEngine;
 
 public class PigeonParticle : MonoBehaviour
 {
     ParticleSystem ps;
+    [SerializeField] GameObject[] pigeonSprites;
+    float timeBeforeComeback = 10f;
+    bool canFlyAway;
     public AudioSource audioSource;
     public AudioClip pigeonsAudioClip;
 
     private void Start()
     {
         ps = GetComponent<ParticleSystem>();
-        audioSource = gameObject.GetComponent<AudioSource>();
-
+        canFlyAway = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-       if(other.gameObject.transform.parent.tag == "Player")
-        {
+       if(canFlyAway && other.gameObject.transform.parent.tag == "Player")
+       {
             ps.Play();
             if(!audioSource.isPlaying)
             {
                 audioSource.PlayOneShot(pigeonsAudioClip, 1);
             }
         }
+            foreach (GameObject p in pigeonSprites) {
+                p.SetActive(false);
+            }
+            canFlyAway = false;
+            StartCoroutine(waitForSeconds(timeBeforeComeback));
+       }
     }
+
+    IEnumerator waitForSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Resetup();
+    }
+
+    void Resetup()
+    {
+        foreach (GameObject p in pigeonSprites)
+        {
+            p.SetActive(true);
+        }
+        ps.gameObject.SetActive(false);
+        ps.gameObject.SetActive(true);
+        canFlyAway = true;
+    }
+
+
+
 }
