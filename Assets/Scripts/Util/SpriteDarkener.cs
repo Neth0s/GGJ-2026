1211c45ec4af6606e0ea2e8961fb6322988cc300
Clemence <geeklego@gmail.com>
2026-02-07 18:51:19 +0100

    new player material to fix sprite darkening

--- Assets/Scripts/Util/SpriteDarkener.cs
+++ Assets/Scripts/Util/SpriteDarkener.cs
@@ -10,6 +10,7 @@ public class SpriteDarkener : MonoBehaviour
     [SerializeField] private float _colorMin = 0.7f;
 
     private Color _initialColor;
+    private Material _material;
     private Coroutine _currentCoroutine = null;
     #endregion
 
@@ -22,13 +23,13 @@ public class SpriteDarkener : MonoBehaviour
         }
         else
         {
-            _initialColor = _spriteRenderer.color;
+            _initialColor = _spriteRenderer.material.color;
         }
     }
 
     public void Darken()
     {
-        _spriteRenderer.color = _initialColor;
+        _spriteRenderer.material.color = _initialColor;
         _currentCoroutine ??= StartCoroutine(DarkenCoroutine());
     }
 
@@ -36,16 +37,16 @@ public class SpriteDarkener : MonoBehaviour
     {
         StopAllCoroutines();
         _currentCoroutine = null;
-        _spriteRenderer.color = _initialColor;
+        _spriteRenderer.material.color = _initialColor;
     }
 
     private IEnumerator DarkenCoroutine()
     {
-        while (_spriteRenderer.color.r > _colorMin)
+        while (_spriteRenderer.material.color.r > _colorMin)
         {
-            float luminosity = _spriteRenderer.color.r - _darkenSpeed;
+            float luminosity = _spriteRenderer.material.color.r - _darkenSpeed;
             Color newColor = new(luminosity, luminosity, luminosity);
-            _spriteRenderer.color = newColor;
+            _spriteRenderer.material.color = newColor;
             yield return new WaitForFixedUpdate();
         }
     }
